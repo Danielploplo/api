c7795c57599d4ab59a5160290841f222dea693cc
Danielposoh <112611180+Danielploplo@users.noreply.github.com>
2022-10-18 15:24:26 +0600

    api

---
+++ drfapp/models.py
@@ -0,0 +1,12 @@
+from tabnanny import verbose
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+class Students(models.Model):
+    first_name = models.CharField(max_length = 50, verbose_name = 'Имя')
+    last_name = models.CharField(max_length = 70, verbose_name = 'Фамилия')
+    age = models.IntegerField(verbose_name = 'Возраст студента')
+    gender = models.CharField(max_length = 50, verbose_name = 'Пол студента')
+    course = models.CharField(max_length = 50, verbose_name = 'Курс')
